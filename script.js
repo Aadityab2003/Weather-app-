aebf5d7020e7bbada5d8949b0cd7a6a823433af4
Aadityab2003 <aadityabaraskar2003@gmail.com>
2025-09-04 17:42:02 +0530

    UI design done and api connected succesfully

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const apiKey="97ce5be0123e2249eb9bcaba9d70dd5c";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?&units=metric&q=bangalore";
 
+
+async function checkWeather(){
+    const response=await fetch(apiUrl + `&appid=${apiKey}`);
+    var data=await response.json();
+
+    console.log(data)
+}
+checkWeather();
