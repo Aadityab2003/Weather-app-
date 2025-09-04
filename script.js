b90defde4c5afa6d44f1bc28f32bef799ea5cdf0
Aadityab2003 <aadityabaraskar2003@gmail.com>
2025-09-04 20:23:56 +0530

    solve errors

--- script.js
+++ script.js
@@ -8,9 +8,12 @@ const weatherIcon=document.querySelector(".weather-icon");
 
 async function checkWeather(city){
     const response=await fetch(apiUrl + city + `&appid=${apiKey}`);
-    var data=await response.json();
 
-    console.log(data)
+    if(response.status==404){
+        document.querySelector(".error").style.display="block";
+        document.querySelector(".weather").style.display="none";
+    }else{
+        var data=await response.json();
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
@@ -29,8 +32,14 @@ async function checkWeather(city){
     else if(data.weather[0].main == "Drizzle"){
         weatherIcon.src="images/drizzle.png";
     }
-    else if(data.weather[0].main == "Mist")
+    else if(data.weather[0].main == "Mist"){
         weatherIcon.src="images/mist.png";
+    }
+
+    document.querySelector(".weather").style.display="block";
+
+    }
+    
 
 }
 
