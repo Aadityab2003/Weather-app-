2a3a96e58ee4b9fdac2d6e39094d3c711c629926
Aadityab2003 <aadityabaraskar2003@gmail.com>
2025-09-04 19:43:44 +0530

    almost done

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 const apiKey="97ce5be0123e2249eb9bcaba9d70dd5c";
-const apiUrl="https://api.openweathermap.org/data/2.5/weather?&units=metric&q=bangalore";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
-
-async function checkWeather(){
-    const response=await fetch(apiUrl + `&appid=${apiKey}`);
+const searchbox=document.querySelector(".search input");
+const searchbtn=document.querySelector(".search button");
+const weatherIcon=document.querySelector(".weather-icon");
+async function checkWeather(city){
+    const response=await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data=await response.json();
 
     console.log(data)
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "clouds.png";
+    }
+    else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "clear.png";
+    }
+    else if(data.weather[0].main == "Rain"){
+        weatherIcon.src="rain.png";
+    }
+    else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src="drizzle.png";
+    }
+    else if(data.weather[0].main == "Mist")
+        weatherIcon.src="mist.png";
+
 }
-checkWeather();
+
+searchbtn.addEventListener("click",()=>{
+    checkWeather(searchbox.value);
+})
+
+
+
